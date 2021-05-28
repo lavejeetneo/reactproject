71d127f66c9ed1c9c8f9a156d0359102b9fb038b
lavejeetneo <lavejeet.singh@neosoftmail.com>
2021-05-28 15:01:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Carousal from "./Carousal";
+import Navbar from "./Navbar";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Carousal/>
+    </div>
+  );
+}
+
+export default App;
