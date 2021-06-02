1fedd197022e83196ee2db4e8030b90b7a6a4a3b
lavejeetneo <lavejeet.singh@neosoftmail.com>
2021-06-02 18:49:07 +0530

    axios prectise

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ import Signup from "./Signup";
 
 import cakeData from "./data.js"
 import Cake from "./Cake";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 function App() {
 
   var [isLogedin, setLogin] = useState(false)
-  
+  var [cakes, setCakes] = useState([])
+
   let data = {
     product : "Cake",
     contact : 9876543210
   }
+
+  let cakeApi = "http://apibyashu.herokuapp.com/api/allcakes"
+
+  useEffect(
+    ()=>{
+      axios({
+        method:"get",
+        url:cakeApi
+      }).then(
+        (res)=>{
+          console.log(res.data);
+          setCakes(res.data.data)
+        },
+        (error)=>{
+          console.error(error);
+        }
+      )
+    }, []
+  )
 
   let LoginDone = ()=>{
     setLogin(true)
@@ -26,15 +47,15 @@ function App() {
       <Carousal/>
       <Signup />
       <Login imformLogin={LoginDone}/>
-      <div className="row">
-        {
-          cakeData.map((each,index)=>{
-            return (
-              <Cake cakeData={each} key={index}/>
-            )
-          })
-        }
-      </div>
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            {
+              cakes.map((each,index)=>{
+                return (
+                  <Cake cakeData={each} key={index}/>
+                )
+              })
+            }
+        </div>
     </div>
   );
 }
--- src/Cake.js
+++ src/Cake.js
@@ -1,11 +1,13 @@
 let Cake = (props)=>{
     console.log(props);
     return (
-        <div className="card" style={{width:"18rem"}}>
-            <img src={props.cakeData.image} className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{props.cakeData.name}</h5>
-                <p className="card-text">{props.cakeData.price}</p>
+        <div class="col">
+            <div className="card" style={{width:"18rem"}}>
+                <img src={props.cakeData.image} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{props.cakeData.name}</h5>
+                    <p className="card-text">{props.cakeData.price}</p>
+                </div>
             </div>
         </div>
     )
